93dfa27d0cacce2041d0d8665c4fc5a321c89ada
tomle1975 <tomle1975@gmail.com>
2023-05-01 09:38:50 -0400

    reverse

---
+++ Verra.Test.Misc/Verra.Test.Misc/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Reverse("Today is sunny and Tom is taking a live coding test."));
+
+static string Reverse(string input)
+{
+    var start = DateTime.Now;
+
+    if (string.IsNullOrEmpty(input) || input.Length == 1) return input;
+
+    var stringBuilder = new StringBuilder();
+    for (var i = input.Length - 1; i >= 0; i--)
+    {
+        stringBuilder.Append(input[i]);
+    }
+
+    Console.WriteLine(DateTime.Now - start);
+
+    return stringBuilder.ToString();
+}
